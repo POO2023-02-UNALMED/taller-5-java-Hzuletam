d16b1eac7459d98a43ff515dd5e8d52d55ff220f
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-09-17 18:14:06 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -8,6 +8,23 @@ public class Ave extends Animal{
     private String  colorPlumas;
 
 
+    public Ave(){
+
+    }
+
+    public Aves(int totalAnimales,String nombre, int edad, String habitat,String genero,Arraylist<Zona> zonas, Arraylist<Ave> lista, int halcones, int aguilas,String colorPlumas){
+        this.totalAnimales= totalAnimales;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.habitat = habitat;
+        this.genero = genero;
+        this.zona = zona;
+        this.listado = lista;
+        this.halcones = halcones;
+        this.aguilas = aguilas;
+        this.colorPlumas = colorPlumas;
+    }
+
     public int cantidadAves(){
         return  listado.lenght();
     }
